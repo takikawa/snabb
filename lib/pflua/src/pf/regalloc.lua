3276c83bc311a88ab54911b7b4d77cb9729e4fe2
Asumu Takikawa <asumu@simplyrobot.org>
2016-10-24 14:59:27 -0700

    Fix regalloc to use real IR data definition
    (this previously used the wrong format based on the pretty
     printed output rather than real IR)

--- lib/pflua/src/pf/regalloc.lua
+++ lib/pflua/src/pf/regalloc.lua
@@ -35,7 +35,7 @@ module(...,package.seeall)
 
 -- Update the ends of intervals based on variable occurrences in
 -- the "control" ast
-local function find_live_in_control(block_num, control, intervals)
+local function find_live_in_control(label, control, intervals)
    -- the head of an ast is always an operation name, so skip
    for i = 2, #control do
       local ast_type = type(control[i])
@@ -43,11 +43,11 @@ local function find_live_in_control(block_num, control, intervals)
       if ast_type == "string" then
 	 for _, interval in ipairs(intervals) do
 	    if control[i] == interval.name then
-	       interval.finish = block_num
+	       interval.finish = label
 	    end
 	 end
       elseif ast_type == "table" then
-	 find_live_in_control(block_num, control[i], intervals)
+	 find_live_in_control(label, control[i], intervals)
       end
    end
 end
@@ -58,25 +58,26 @@ end
 -- "bindings" for a variable and ends at the last reference.
 --
 -- A live interval is a table
---   { name = String, start = Num, finish = Num }
+--   { name = String, start = String, finish = String }
 --
--- The IR is implicitly numbered by the array indices in
--- "blocks" which corresponds to depth-first order.
+-- The start and finish fields are block labels, and they are
+-- traversed according to the IR order (depth-first).
 --
 local function compute_live_intervals(ssa)
    local live_intervals = {}
-   local blocks = ssa[3]
 
-   for i = 2, #blocks do
-      local bindings = blocks[i][3]
-      local live = {}
+   for _, label in ipairs(ssa.order) do
+      local block = ssa.blocks[label]
+      local bindings = block.bindings
 
-      for j = 2, #bindings do
-	 local interval = { name = bindings[j][1], start = i-1, finish = nil }
+      for _, binding in pairs(bindings) do
+	 local interval = { name = binding.name,
+			    start = label,
+			    finish = nil }
 	 table.insert(live_intervals, interval)
       end
 
-      find_live_in_control(i-1, blocks[i][4], live_intervals)
+      find_live_in_control(label, block.control, live_intervals)
    end
 
    return live_intervals
@@ -100,101 +101,81 @@ end
 function selftest()
    -- "ip"
    local example_1 =
-      { "ssa",
-	{ "start", "L1" },
-	{ "blocks",
-	  { "block",
-	    { "label", "L1" },
-	    { "bindings" },
-	    { "control",
-	      { "if", { ">=", "len", 14 }, "L4", "L5" } } },
-	  { "block",
-	    { "label", "L4" },
-	    { "bindings" },
-	    { "control",
-	      { "return",
-		{ "=", { "[]", 12, 2 }, 8 } } } },
-	  { "block",
-	    { "label", "L5" },
-	    { "bindings" },
-	    { "control", { "return", { "false" } } } } } }
+      { start = "L1",
+	order = { "L1", "L4", "L5" },
+	blocks =
+	   { L1 = { label = "L1",
+		    bindings = {},
+		    control = { "if", { ">=", "len", 14 }, "L4", "L5" } },
+	     L4 = { label = "L4",
+		    bindings = {},
+		    control = { "return", { "=", { "[]", 12, 2 }, 8 } } },
+	     L5 = { label = "L5",
+		    bindings = {},
+		    control = { "return", { "false " } } } } }
 
    assert(#compute_live_intervals(example_1) == 0)
 
    local example_2 =
-      { "ssa",
-	{ "start", "L1" },
-	{ "blocks",
-	  { "block",
-	    { "label", "L1" },
-	    { "bindings" },
-	    { "control", { "if", { ">=", "len", 34 }, "L4", "L5" } } },
-	  { "block",
-	    { "label", "L4" },
-	    { "bindings", { "v1", { "[]", 12, 2 } } },
-	    { "control",
-	      { "if", { "=", "v1", 8 }, "L6", "L7" } } },
-	  { "block",
-	    { "label", "L6" },
-	    { "bindings" },
-	    { "control", { "return", { "=", { "[]", 23, 1 }, 6 } } } },
-	  { "block",
-	    { "label", "L7" },
-	    { "bindings" },
-	    { "control", { "if", { ">=", "len", 54 }, "L8", "L9" } } },
-	  { "block",
-	    { "label", "L8" },
-	    { "bindings" },
-	    { "control", { "if", { "=", "v1", 56710 }, "L10", "L11" } } },
-	  { "block",
-	    { "label", "L10" },
-	    { "bindings", { "v2", { "[]", 20, 1 } } },
-	    { "control", { "if", { "=", "v2", 6 }, "L12", "L13" } } },
-	  { "block",
-	    { "label", "L12" },
-	    { "bindings" },
-	    { "control", { "return", { "true" } } } },
-	  { "block",
-	    { "label", "L13" },
-	    { "bindings" },
-	    { "control", { "if", { ">=", "len", 55 }, "L14", "L15" } } },
-	  { "block",
-	    { "label", "L14" },
-	    { "bindings" },
-	    { "control", { "if", { "=", "v2", 44 }, "L16", "L17" } } },
-	  { "block",
-	    { "label", "L16" },
-	    { "bindings" },
-	    { "control", { "return", { "=", { "[]", 54, 1 }, 6 } } } },
-	  { "block",
-	    { "label", "L17" },
-	    { "bindings" },
-	    { "control", { "return", { "false" } } } },
-	  { "block",
-	    { "label", "L15" },
-	    { "bindings" },
-	    { "control", { "return", { "false" } } } },
-	  { "block",
-	    { "label", "L11" },
-	    { "bindings" },
-	    { "control", { "return", { "false" } } } },
-	  { "block",
-	    { "label", "L9" },
-	    { "bindings" },
-	    { "control", { "return", { "false" } } } },
-	  { "block",
-	    { "label", "L5" },
-	    { "bindings" },
-	    { "control", { "return", { "false" } } } } } }
+      { start = "L1",
+	order = { "L1", "L4", "L6", "L7", "L8", "L10", "L12", "L13",
+		  "L14", "L16", "L17", "L15", "L11", "L9", "L5" },
+	blocks =
+	   { L1 = { label = "L1",
+	            bindings = {},
+	            control = { "if", { ">=", "len", 34 }, "L4", "L5" } },
+	     L4 = { label = "L4",
+	            bindings = { { name = "v1", value = { "[]", 12, 2 } } },
+	            control = { "if", { "=", "v1", 8 }, "L6", "L7" } },
+	     L6 = { label = "L6",
+	            bindings = {},
+	            control = { "return", { "=", { "[]", 23, 1 }, 6 } } },
+	     L7 = { label = "L7",
+	            bindings = {},
+	            control = { "if", { ">=", "len", 54 }, "L8", "L9" } },
+	     L8 = { label = "L8",
+	            bindings = {},
+	            control = { "if", { "=", "v1", 56710 }, "L10", "L11" } },
+	     L10 = { label = "L10",
+	             bindings = { { name = "v2", value = { "[]", 20, 1 } } },
+	             control = { "if", { "=", "v2", 6 }, "L12", "L13" } },
+	     L12 = { label = "L12",
+	             bindings = {},
+	             control = { "return", { "true" } } },
+	     L13 = { label = "L13",
+	             bindings = {},
+	             control = { "if", { ">=", "len", 55 }, "L14", "L15" } },
+	     L14 = { label = "L14",
+	             bindings = {},
+	             control = { "if", { "=", "v2", 44 }, "L16", "L17" } },
+	     L16 = { label = "L16",
+	             bindings = {},
+	             control = { "return", { "=", { "[]", 54, 1 }, 6 } } },
+	     L17 = { label = "L17",
+	             bindings = {},
+	             control = { "return", { "false" } } },
+	     L15 = { label = "L15",
+	             bindings = {},
+	             control = { "return", { "false" } } },
+	     L11 = { label = "L11",
+	             bindings = {},
+	             control = { "return", { "false" } } },
+	     L9 = { label = "L9",
+	            bindings = {},
+	            control = { "return", { "false" } } },
+	     L5 = { label = "L5",
+	            bindings = {},
+	            control = { "return", { "false" } } } } }
 
    local intervals_2 = compute_live_intervals(example_2)
+   print(#intervals_2)
    assert(#intervals_2 == 2)
    assert(intervals_2[1].name == "v1")
-   assert(intervals_2[1].start == 2)
-   assert(intervals_2[1].finish == 5)
+   assert(intervals_2[1].start == "L4")
+   assert(intervals_2[1].finish == "L8")
    assert(intervals_2[2].name == "v2")
-   assert(intervals_2[2].start == 6)
-   assert(intervals_2[2].finish == 9)
+   assert(intervals_2[2].start == "L10")
+   assert(intervals_2[2].finish == "L14")
 
    -- "ip[2:2] * 3 = 0"
    example_3 = { "ssa",
